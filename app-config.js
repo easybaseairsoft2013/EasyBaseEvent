04de8aee829bf060a09e10ac075611d884f5a960
easybaseairsoft2013 <tableteasybaseairsoft@gmail.com>
2025-08-17 07:56:27 +0200

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,1 +1,6 @@
-
+<script>
+window.APP_CONFIG = {
+  supabaseUrl: 'https://vsrktbsqiuoeenyvifiq.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzcmt0YnNxaXVvZWVueXZpZmlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzOTQxNTAsImV4cCI6MjA3MDk3MDE1MH0.OGs5X5i__Fr-LBSDN8uRGXM9YKCKJSaz_VT_BIPiDug'
+};
+</script>
