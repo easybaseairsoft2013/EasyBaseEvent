8102c8e20853c7397939fb1d16f5e77f6a5a5700
easybaseairsoft2013 <tableteasybaseairsoft@gmail.com>
2025-08-17 07:57:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', () => {}); // minimál SW – HTTPS kell (GitHub Pages ad)
